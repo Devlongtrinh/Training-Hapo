d8d0cebcc9207c7bfb55c34cf8f57f120d93f92c
Trịnh Quang Long <tenlong201@gmail.com>
2022-07-19 10:14:04 +0700

    Fix up-down

--- database/migrations/2022_07_15_031318_update_users_table.php
+++ database/migrations/2022_07_15_031318_update_users_table.php
@@ -14,6 +14,7 @@ class UpdateUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->increments('id')->change();
             $table->string('user_name');
             $table->integer('role')->nullable();
             $table->datetime('d_o_b')->nullable();
@@ -30,6 +31,14 @@ class UpdateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('id');
+            $table->dropColumn('user_name');
+            $table->dropColumn('role');
+            $table->dropColumn('d_o_b');
+            $table->dropColumn('phone');
+            $table->dropColumn('address');
+            $table->dropColumn('deleted_at');
+        });
     }
 }
