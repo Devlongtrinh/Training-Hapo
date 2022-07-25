a3f16ba807955671015986102d221d5d29de1853
Trịnh Quang Long <tenlong201@gmail.com>
2022-07-25 08:54:17 +0700

    Create First

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Course;
+use App\Models\Lesson;
+use App\Models\CourseUser;
+use App\Models\Review;
 
 class HomeController extends Controller
 {
@@ -13,6 +17,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $mainCourses = Course::main()->get();
+        $otherCourses = Course::other()->orderBy('id', config('course.course_sort_descending'))->get();
+        $reviews = Review::get();
+        $countCourses = Course::count();
+        $countLessons = Lesson::count();
+        $countLearners = CourseUser::countRegistered() -> Learners();
+
+        return view('home', compact('mainCourses', 'otherCourses', 'reviews', 'learners', 'countCourse', 'countLession'));
     }
 }
