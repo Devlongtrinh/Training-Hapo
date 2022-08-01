ef68df05b3fc3215af20cacefcf6928651616079
Trịnh Quang Long <tenlong201@gmail.com>
2022-08-01 11:28:09 +0700

    Fix

--- database/seeders/ReviewsTableSeeder.php
+++ database/seeders/ReviewsTableSeeder.php
@@ -14,6 +14,6 @@ class ReviewsTableSeeder extends Seeder
      */
     public function run()
     {
-        Review::factory()->count(202)->create();
+        Review::factory()->count(20)->create();
     }
 }
